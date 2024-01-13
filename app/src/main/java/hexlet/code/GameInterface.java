bd1e0de57ba2b2292804d485b42310fbdee08519
Oleg Makarov <makarovolegbox@yandex.ru>
2024-01-13 17:51:13 +0300

    Changes App.java Unversioned Files C:\Users\PC SAN\IdeaProjects\java-project-61\app\.idea\.gitignore C:\Users\PC SAN\IdeaProjects\java-project-61\app\.gitignore C:\Users\PC SAN\IdeaProjects\java-project-61\app\build.gradle.kts C:\Users\PC SAN\IdeaProjects\java-project-61\app\.idea\gradle.xml C:\Users\PC SAN\IdeaProjects\java-project-61\app\gradle\wrapper\gradle-wrapper.jar C:\Users\PC SAN\IdeaProjects\java-project-61\app\gradle\wrapper\gradle-wrapper.properties C:\Users\PC SAN\IdeaProjects\java-project-61\app\gradlew C:\Users\PC SAN\IdeaProjects\java-project-61\app\gradlew.bat C:\Users\PC SAN\IdeaProjects\java-project-61\app\.idea\misc.xml C:\Users\PC SAN\IdeaProjects\java-project-61\app\settings.gradle.kts C:\Users\PC SAN\IdeaProjects\java-project-61\app\.idea\vcs.xml

--- app/src/main/java/hexlet/code/GameInterface.java
+++ app/src/main/java/hexlet/code/GameInterface.java
@@ -5,21 +5,74 @@ import java.util.Scanner;
 public class GameInterface {
 
 
-
-    public static void playGame (int numberOfGame){
+    public static void playGame(int numberOfGame) {
         int winCount = 0;
         int maxWins = 3;
         String userName;
         String question;
         String rightAnswer;
         String usersAnswer;
-        String []gameInfo = new String[1];
+        String[] gameInfo = new String[1];
 
         Scanner getUsersAnswer = new Scanner(System.in);
 
         userName = Cli.greetings();
 
-        switch (numberOfGame){
+        showGameTasToUser(numberOfGame);
+
+
+        while (winCount < maxWins) {
+
+            switch (numberOfGame) {
+
+
+                case 2:
+                    gameInfo = EvenGame.playEvenGame();
+                    break;
+                case 3:
+
+                    gameInfo = CalcGame.playCalcGame();
+                    break;
+                case 4:
+                    gameInfo = GCD_Game.playGCD_Game();
+                    break;
+                case 5:
+                    gameInfo = ProgressionGame.playProgressionGame();
+                    break;
+                case 6:
+                    gameInfo = PrimeGame.PlayPrimeGame();
+                    break;
+
+            }
+
+            question = gameInfo[0];
+            rightAnswer = gameInfo[1];
+
+            System.out.println("Question: " + question);
+            System.out.println("Your answer:");
+            usersAnswer = getUsersAnswer.next();
+
+            if (usersAnswer.equals(rightAnswer)) {
+                System.out.println("Correct!");
+                winCount++;
+            } else {
+                System.out.println("'" + usersAnswer + "' is wrong answer ;(. Correct answer was '" + rightAnswer +
+                        "'.\nLet's try again, " + userName);
+                break;
+
+            }
+
+
+            if (winCount == maxWins) {
+                System.out.println("Congratulations, " + userName + "!");
+            }
+        }
+
+    }
+
+    public static void showGameTasToUser(int numberOfGame) {
+
+        switch (numberOfGame) {
 
             case 2:
                 EvenGame.showTask();
@@ -37,55 +90,5 @@ public class GameInterface {
                 PrimeGame.showTask();
                 break;
         }
-
-
-        while (winCount < maxWins){
-
-            switch (numberOfGame){
-
-
-            case 2:
-                gameInfo = EvenGame.playEvenGame();
-                break;
-            case 3:
-
-                gameInfo = CalcGame.playCalcGame();
-                break;
-            case 4:
-                gameInfo = GCD_Game.playGCD_Game();
-                break;
-            case 5:
-                gameInfo = ProgressionGame.playProgressionGame();
-                break;
-            case 6:
-                gameInfo = PrimeGame.PlayPrimeGame();
-                break;
-
-        }
-
-            question = gameInfo[0];
-            rightAnswer = gameInfo[1];
-
-          System.out.println("Question: " + question);
-          System.out.println("Your answer:");
-          usersAnswer = getUsersAnswer.next();
-
-            if (usersAnswer.equals(rightAnswer)){
-                System.out.println("Correct!");
-                winCount ++;
-            }
-            else {
-                System.out.println("'" + usersAnswer + "' is wrong answer ;(. Correct answer was '" + rightAnswer +
-                        "'.\nLet's try again, " + userName);
-                break;
-
-            }
-
-
-        if (winCount == maxWins){
-            System.out.println("Congratulations, " + userName +"!");
-        }
-        }
-
-}
+    }
 }
